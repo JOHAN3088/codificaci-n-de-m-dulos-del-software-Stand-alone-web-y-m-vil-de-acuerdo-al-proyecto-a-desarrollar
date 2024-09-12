513ea39269412463c2ed29012d74dc662f38749f
JOHAN3088 <johan3088@hotmail.com>
2024-09-12 15:24:01 -0500

    validaciones actualizacion

--- src/main/java/com/proyectosena/medicamentos/modelo/medicamento.java
+++ src/main/java/com/proyectosena/medicamentos/modelo/medicamento.java
@@ -13,21 +13,27 @@ import lombok.Data;
  *
  * @author User
  */
+/**
+ * Length----Longitud de los datos
+ * Unique----Aplicacion de entidad
+ * Nullable--El campo no sea nulo
+ * @author User
+ */
  @Entity
  @Data
 public class medicamento {
      //CONFIRME LA LLAVE PRIMARIA Y LUEGO LOS ATRIBUTOS
-      @Id
+     @Id
       //se pueden identificar las columnas,especificar el tipo de contenido,las caracteristicas
-     @Column
+     @Column(name="IdCodigo")
      private long idcodigo;
-     @Column
+     @Column(name="nombre",nullable=false,length=20,unique=true)
      private String nombre;
-     @Column
+     @Column(nullable=false)
      private String gramaje;
-     @Column
+     @Column(nullable=false)
      private String caracteristicas;
-     @Column
+     @Column(nullable=false)
      private long existencias;
      
 }
